e3edc827b8573133c876365026fae9a902cf05cb
Nate Wessel <bike756@gmail.com>
2019-12-30 16:46:12 -0500

    making things pretty

--- cypress/integration/basic_spec.js
+++ cypress/integration/basic_spec.js
@@ -1,10 +1,13 @@
-describe('Login', ()=>{
-	before(()=>{ cy.login() });
-	after(()=>{ cy.logout() });
+describe('Login', () => {
+  before(() => {
+    cy.login()
+  })
+  after(() => {
+    cy.logout()
+  })
 
-	it('Visit existing region', ()=>{
-		cy.contains('Nate - testing UI').click();
-		cy.get('legend').contains('Nate - testing UI');
-	})
-
+  it('Visit existing region', () => {
+    cy.contains('Nate - testing UI').click()
+    cy.get('legend').contains('Nate - testing UI')
+  })
 })

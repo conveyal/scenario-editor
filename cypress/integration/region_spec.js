61b3923184686160f12f20662f87cf7bd81ebd6b
Nate Wessel <bike756@gmail.com>
2020-01-02 11:43:38 -0500

    building out regions tests

--- cypress/integration/region_spec.js
+++ cypress/integration/region_spec.js
@@ -9,20 +9,35 @@ describe('Set up a new region', () => {
     cy.get('input[name="Region Name"]')
   })
 
-  it('search for new region locations', () => {
-    // try several selected region
-    cy.fixture('regions.json').then(regions => {
-      regions.forEach(region => {
+  it('search for several locations by name', () => {
+    // try searching all selected regions
+    cy.fixture('regions.json').then(JSON => {
+      JSON.regions.forEach(region => {
         cy.get('input#react-select-2-input').focus()
           .type('{selectall}{backspace}')
           .type(region.searchTerm)
         cy.contains(region.foundName).click({force:true})
+        // TODO check that the map moves
+        cy.wait(2000)
+        cy.screenshot(region.searchTerm+'-search-result.png')
       })
     })
   })
 
-  it('enter values', () => {
+  it('enter valid and invalid coordinates', ()=>{
+    // coordinate inputs must be valid
+    cy.get('input[name="North bound"]')
+  })
+
+  it('enter region descriptors', () => {
+    // name and decription
     cy.get('input[name="Region Name"]').type('Temporary region')
     cy.get('input[name="Description"]').type('this should be deleted shortly')
+  })
+
+  it('select pbf', () => {
+    cy.get('input[type=file]')
+    // TODO file uploads seem difficult with Cypress..
+    // see: https://www.npmjs.com/package/cypress-file-upload
   })
 })

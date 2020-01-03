547efcd623238324880dd89f81a15ad6fe48e65a
Nate Wessel <bike756@gmail.com>
2020-01-03 13:05:36 -0500

    trying to check for presence of map global var

--- cypress/integration/region_spec.js
+++ cypress/integration/region_spec.js
@@ -3,37 +3,61 @@ describe('Set up a new region', () => {
     cy.login()
   })
 
-  it('open new region page', () => {
+  it('open "new region" page', () => {
     cy.visit('/')
     cy.contains('Set up a new region').click()
+    // has form
     cy.get('input[name="Region Name"]')
+    cy.get('div.leaflet-container')
+    // has map loaded
+    // TODO STILL not working...
+    cy.waitUntil(
+      () =>
+        cy
+          .window({log: false})
+          .then(
+            w =>
+              w.hasOwnProperty('LeafletMap') &&
+              typeof w.LeafletMap !== 'undefined'
+          ),
+      {timeout: 10000, description: 'Map Global is available'}
+    )
+    cy.window().then(w => {
+      console.log(w.LeafletMap)
+    })
   })
 
-  it('search for several locations by name', () => {
+  it('search for map location by name', () => {
     // try searching all selected regions
     cy.fixture('regions.json').then(JSON => {
       JSON.regions.forEach(region => {
-        cy.get('input#react-select-2-input').focus()
-          .type('{selectall}{backspace}')
+        cy.get('input#react-select-2-input')
+          .focus()
+          .clear()
           .type(region.searchTerm)
-        cy.contains(region.foundName).click({force:true})
-        // TODO check that the map moves
-        cy.wait(2000)
-        cy.screenshot(region.searchTerm+'-search-result.png')
+        cy.contains(region.foundName).click({force: true})
+        cy.window().then(w => {
+          console.log('center is ' + w.LeafletMap.getCenter())
+        })
+        cy.screenshot(region.searchTerm + '-search-result.png')
       })
     })
   })
 
-  it('enter valid and invalid coordinates', ()=>{
+  it('enter valid and invalid coordinates', () => {
     // coordinate inputs must be valid
+    // TODO not finished
     cy.get('input[name="North bound"]')
+      .clear()
+      .type(45.769)
+    cy.get('a[name="Set up a new region"]').should('be.disabled')
   })
 
-  it('enter region descriptors', () => {
-    // name and decription
-    cy.get('input[name="Region Name"]').type('Temporary region')
-    cy.get('input[name="Description"]').type('this should be deleted shortly')
-  })
+  //  it('enter region descriptors', () => {
+  //    // name and decription
+  //    cy.get('input[name="Region Name"]').type('Temporary region')
+  //    cy.get('input[name="Description"]').type('this should be deleted shortly')
+  //  })
 
   it('select pbf', () => {
     cy.get('input[type=file]')

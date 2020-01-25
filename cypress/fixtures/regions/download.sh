28f638474bd246b084bc3519edf4dd8fd305654c
Nate Wessel <bike756@gmail.com>
2020-01-25 10:33:21 -0500

    removed cincinnati for compatibility with existing test instructions

--- cypress/fixtures/regions/download.sh
+++ cypress/fixtures/regions/download.sh
@@ -14,19 +14,3 @@ wget -O portland/streets.osm --post-file=portland/overpass-query.txt https://ove
 # convert to xml
 osmium cat --overwrite portland/streets.osm -o portland/streets.osm.pbf
 rm portland/streets.osm
-
-
-
-# CINCINNATI
-
-# download SORTA GTFS
-wget https://www.go-metro.com/uploads/GTFS/google_transit_info.zip -O cincinnati/SORTA.gtfs.zip
-
-# download TANK GTFS
-wget https://www.tankbus.org/Portals/tankbus/gtfs/tank-google-transit.zip -O cincinnati/TANK.gtfs.zip
-
-# download Cincy OSM XML
-wget -O cincinnati/streets.osm --post-file=cincinnati/overpass-query.txt https://overpass-api.de/api/interpreter
-# convert to xml
-osmium cat --overwrite cincinnati/streets.osm -o cincinnati/streets.osm.pbf
-rm cincinnati/streets.osm

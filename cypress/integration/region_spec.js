a99f2eddf4ad11fc73e44ae90341588556695a59
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-01-28 11:29:08 +0800

    Add mapIsReady command to Cypress

--- cypress/integration/region_spec.js
+++ cypress/integration/region_spec.js
@@ -1,50 +1,39 @@
-describe('Set up a new region', () => {
-  before(() => {
+describe('Set up a new region', function() {
+  before(function() {
     cy.login()
   })
 
-  it('open "new region" page', () => {
+  it('open "new region" page', function() {
     cy.visit('/')
     cy.contains('Set up a new region').click()
     // has form
     cy.get('input[name="Region Name"]')
-    cy.get('div.leaflet-container')
-    // has map loaded
-    // TODO STILL not working...
-    cy.waitUntil(
-      () =>
-        cy.window({log: false}).then(w => typeof w.LeafletMap !== 'undefined'),
-      {timeout: 10000, description: 'Map Global is available'}
-    )
-    cy.window().then(w => {
-      console.log(w.LeafletMap)
-    })
   })
 
-  it('search for map location by name', () => {
+  it('search for map location by name', function() {
+    cy.visit('/regions/create')
+    cy.mapIsReady()
     // try searching all selected regions
     cy.fixture('regions.json').then(JSON => {
-      JSON.regions.forEach(region => {
+      cy.wrap(JSON.regions).each(region => {
         cy.get('input#react-select-2-input')
           .focus()
           .clear()
           .type(region.searchTerm)
         cy.contains(region.foundName).click({force: true})
-        cy.window().then(w => {
-          console.log('center is ' + w.LeafletMap.getCenter())
-        })
         cy.screenshot(region.searchTerm + '-search-result.png')
       })
     })
   })
 
   it('enter valid and invalid coordinates', () => {
+    cy.visit('/regions/create')
     // coordinate inputs must be valid
     // TODO not finished
     cy.get('input[name="North bound"]')
       .clear()
       .type(45.769)
-    cy.get('a[name="Set up a new region"]').should('be.disabled')
+    cy.get('a[name="Set up a new region"]').should('have.attr', 'disabled')
   })
 
   //  it('enter region descriptors', () => {
@@ -54,6 +43,7 @@ describe('Set up a new region', () => {
   //  })
 
   it('select pbf', () => {
+    cy.visit('/regions/create')
     cy.get('input[type=file]')
     // TODO file uploads seem difficult with Cypress..
     // see: https://www.npmjs.com/package/cypress-file-upload

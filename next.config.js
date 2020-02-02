1b9dd0cccd185891ceb9c76b85e1e25fb3870476
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-02-03 07:39:24 +0800

    Add default environment variables
    Analysis UI can now start with just a Mapbox Access Token

--- next.config.js
+++ next.config.js
@@ -14,21 +14,22 @@ if (process.env.API_URL === undefined) {
   require('dotenv').config({path: '.env.build'})
 }
 
+// Add defaults for AUTH0 if auth is disabled
+const AUTH_DISABLED = process.env.AUTH_DISABLED === 'true'
+
 const env = {
   ADMIN_ACCESS_GROUP: process.env.ADMIN_ACCESS_GROUP || 'conveyal',
-  API_URL: process.env.API_URL,
-  AUTH_DISABLED: process.env.AUTH_DISABLED === 'true',
-  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  API_URL: process.env.API_URL || 'http://localhost:3000',
+  AUTH_DISABLED,
+  AUTH0_CLIENT_ID: AUTH_DISABLED ? 'unrequired' : process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: AUTH_DISABLED ? 'unrequired' : process.env.AUTH0_DOMAIN,
   LOGROCKET: process.env.LOGROCKET || false,
   MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN
 }
 
 module.exports = phase => {
   if (phase === PHASE_PRODUCTION_BUILD) {
-    if (
-      Object.values(env).findIndex(v => v === undefined || v === null) !== -1
-    ) {
+    if (Object.values(env).find(v => v === undefined || v === null)) {
       console.error(
         ```
 Please ensure required environment variables can be found. If running locally,

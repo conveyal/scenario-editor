dff19b89c7b186f06e00275c23d0aa0c069e41a7
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-02-06 15:04:52 +0800

    Update changelog

--- pages/changelog/index.js
+++ pages/changelog/index.js
@@ -1,41 +1,59 @@
+import {Box, Divider, Heading, Link, Stack} from '@chakra-ui/core'
 import React from 'react'
 
 import C201909 from './201909.mdx'
 import C201910 from './201910.mdx'
+import C20200210 from './20200210.mdx'
 
 const changes = [
-  ['September 13th, 2019', C201909],
-  ['October 12th, 2019', C201910]
+  ['February 10th, 2020', C20200210],
+  ['October 12th, 2019', C201910],
+  ['September 13th, 2019', C201909]
 ]
 
 export default function Changelog() {
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col-sm-8 col-xs-12'>
-          <h1>
-            Changelog{'    '}
-            <small>
-              <a href='/'>Back to Analysis</a>
-            </small>
-          </h1>
-          <hr />
-        </div>
-      </div>
+    <Stack spacing={10} p={20}>
+      <Heading size='2xl'>
+        Conveyal Analysis Changelog{' '}
+        <Link color='blue.500' href='/'>
+          Back to Analysis
+        </Link>
+      </Heading>
       {changes.map(([title, C], i) => (
-        <div className='row' key={i}>
-          <div className='col-sm-8 col-xs-12'>
-            <div className='panel panel-info'>
-              <div className='panel-heading'>
-                <h5 className='panel-title'>{title}</h5>
-              </div>
-              <div className='panel-body'>
-                <C />
-              </div>
-            </div>
-          </div>
-        </div>
+        <Box key={i}>
+          <Heading mb={5} size='xl'>
+            {title}
+          </Heading>
+          <Box className='CL'>
+            <C />
+          </Box>
+        </Box>
       ))}
-    </div>
+      <style jsx global>{`
+        .CL h2 {
+          font-size: 2rem;
+          margin-bottom: 1rem;
+        }
+
+        .CL h3 {
+          font-size: 1.5rem;
+          margin-bottom: 0.75rem;
+        }
+
+        .CL p {
+          margin-bottom: 1rem;
+        }
+
+        .CL ul {
+          padding-left: 1.5rem;
+          margin-bottom: 1rem;
+        }
+
+        .CL img {
+          display: inline-block;
+        }
+      `}</style>
+    </Stack>
   )
 }

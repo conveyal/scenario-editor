a26380840c7feb73f8a30fa8f811524d0d103e86
Nate Wessel <bike756@gmail.com>
2020-04-06 11:18:39 -0400

    conditionally set up region

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -32,6 +32,46 @@ Cypress.Cookies.defaults({
   whitelist: ['user']
 })
 
+Cypress.Commands.add('setupRegion', regionName => {
+  // set up the named region if it doesn't already exist
+  let name = 'autogen ' + regionName
+  cy.visit('/')
+  cy.get('body').then(body => {
+    if (body.text().includes(name)) {
+      cy.findByText(name).click()
+      cy.location('pathname').should('match', /\/regions\/.{24}/)
+    } else {
+      cy.visit('/regions/create')
+      cy.findByPlaceholderText('Region Name').type(name, {delay: 1})
+      cy.fixture('regions/' + regionName + '.json').then(region => {
+        cy.findByLabelText(/North bound/)
+          .clear()
+          .type(region.north, {delay: 1})
+        cy.findByLabelText(/South bound/)
+          .clear()
+          .type(region.south, {delay: 1})
+        cy.findByLabelText(/West bound/)
+          .clear()
+          .type(region.west, {delay: 1})
+        cy.findByLabelText(/East bound/)
+          .clear()
+          .type(region.east, {delay: 1})
+        // Select PBF file
+        cy.fixture(region.PBFfile, {encoding: 'base64'}).then(fileContent => {
+          cy.get('input[type="file"]').upload({
+            encoding: 'base64',
+            fileContent,
+            fileName: region.PBFfile,
+            mimeType: 'application/octet-stream'
+          })
+        })
+      })
+      cy.findByRole('button', {name: /Set up a new region/}).click()
+      cy.location('pathname').should('match', /\/regions\/.{24}/)
+    }
+  })
+})
+
 Cypress.Commands.add('mapIsReady', () => {
   cy.window().should('have.property', 'LeafletMap')
   // map should have a tileLayer which is done loading

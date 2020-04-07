152c4b9c63ddc091340b8f63d369ebee6e6babac
Nate Wessel <bike756@gmail.com>
2020-04-07 11:25:58 -0400

    confirm bundle saved

--- cypress/integration/local/bundles.js
+++ cypress/integration/local/bundles.js
@@ -5,10 +5,11 @@ context('GTFS bundles', () => {
   })
 
   it('can be uploaded with single feed', function() {
+    let bundleName = 'single-GTFS bundle'
     cy.findByTitle('GTFS Bundles').click({force: true})
     cy.findByText(/Create a bundle/).click()
     cy.location('pathname').should('match', /.*\/bundles\/create$/)
-    cy.findByLabelText(/Bundle Name/i).type('single GTFS bundle')
+    cy.findByLabelText(/Bundle Name/i).type(bundleName)
     cy.fixture(this.region.GTFSfile, {encoding: 'base64'}).then(fileContent => {
       cy.get('input[type="file"]').upload({
         encoding: 'base64',
@@ -20,6 +21,15 @@ context('GTFS bundles', () => {
     cy.findByRole('button', {name: /Create/i}).click()
     cy.findByText(/Processing/)
     cy.location('pathname', {timeout: 30000}).should('match', /\/bundles$/)
-    // TODO verify the upload is selectable from the dropdown
+    // confirm that the bundle was saved
+    cy.contains('or select an existing one')
+    cy.findByText(/Select.../).click()
+    cy.findByText(bundleName).click()
+    cy.findByLabelText(/Bundle Name/)
+      .invoke('val')
+      .then(name => {
+        expect(name).to.equal(bundleName)
+      })
+    cy.findByText(/Delete this bundle/i).click()
   })
 })

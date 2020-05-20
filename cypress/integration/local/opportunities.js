1f96008e4cdaa74fe737f1edb65e9247fdcf4288
Nate Wessel <bike756@gmail.com>
2020-05-20 12:32:17 -0400

    more opp data in fixture

--- cypress/integration/local/opportunities.js
+++ cypress/integration/local/opportunities.js
@@ -9,34 +9,41 @@ describe('Opportunities', () => {
   })
 
   it('can be uploaded as CSV', function () {
+    let opportunity = this.region.opportunities.csv
+
     let oppName = 'opp ' + Date.now()
     cy.findByText(/Upload a new dataset/i).click()
     cy.location('pathname').should('match', /\/opportunities\/upload$/)
     cy.findByPlaceholderText(/^Opportunity dataset/i).type(oppName)
-    cy.fixture(this.region.opportunityCSV).then((fileContent) => {
+    cy.fixture(opportunity.file).then((fileContent) => {
       cy.findByLabelText(/^Select opportunity dataset/i).upload({
         fileContent,
-        fileName: this.region.opportunityCSV,
+        fileName: opportunity.file,
         mimeType: 'text/csv',
         encoding: 'utf-8'
       })
     })
-    cy.findByLabelText(/Latitude/).type('lat')
-    cy.findByLabelText(/Longitude/).type('lon')
+    cy.findByLabelText(/Latitude/).type(opportunity.latitudeField)
+    cy.findByLabelText(/Longitude/).type(opportunity.longitudeField)
     cy.get('a.btn')
       .contains(/Upload/)
       .should('not.be.disabled')
       .click()
     cy.location('pathname').should('match', /opportunities$/)
+    // find the alert showing this upload is complete and close it
     cy.contains(new RegExp(oppName + ' \\(DONE\\)'), {timeout: 10000})
+      .parent()
+      .parent()
+      .findByRole('button', /x/)
+      .click()
+    //
     cy.findByText(/Select\.\.\./)
       .click()
       .type(`${oppName}{enter}`)
     cy.contains(/Delete entire dataset/i).click()
     // TODO need to finish by checking that the upload:
-    // has only the one numeric field
+    // has only the numeric fields
     // can be seen on the map
-    // can be deleted
   })
 
   it('can be imported from LODES', function () {

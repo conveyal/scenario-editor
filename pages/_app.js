8a7addbdc63b8c7817f0a6be5afdb101cedc1f8d
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-05-22 17:16:01 +0800

    Track UI performance

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import React from 'react'
 
 import ChakraTheme from '../lib/chakra'
 import {ErrorModal} from '../lib/components/error-modal'
+import ga from '../lib/ga'
 import LogRocket from '../lib/logrocket'
 
 import 'react-datetime/css/react-datetime.css'
@@ -42,4 +43,18 @@ export default class ConveyalApp extends App {
       </ChakraTheme>
     )
   }
+}
+
+/**
+ * Track UI performance. Learn more here: https://nextjs.org/docs/advanced-features/measuring-performance
+ */
+export function reportWebVitals({id, name, label, value}) {
+  ga('send', 'event', {
+    eventCategory:
+      label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    eventAction: name,
+    eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    eventLabel: id, // id unique to current page load
+    nonInteraction: true // avoids affecting bounce rate.
+  })
 }

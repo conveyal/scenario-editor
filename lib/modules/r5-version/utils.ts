384950e9a9c121444c0ba0d6c52ef43baa60840d
Trevor Gerhardt <trevorgerhardt@gmail.com>
2021-02-24 22:18:33 -0600

    Update tests

--- lib/modules/r5-version/utils.ts
+++ lib/modules/r5-version/utils.ts
@@ -6,25 +6,42 @@ import {
 
 /**
  * Convert an r5 version to a number for comparing
+ * NB Assumes no major, minor, patch, or commit version exceeds 100
  */
-export function versionToNumber(version = ''): number {
-  const versionMatch =
-    version.match(/\./g).length == 1
-      ? version.match(VERSION_PARSE_REGEX)
-      : version.match(OLD_VERSION_PARSE_REGEX)
-  if (versionMatch) {
-    const [, major = '0', minor = '0', patch = '0', commit = '0'] = versionMatch
+export function versionToNumber(version?: string): number {
+  const versionString = version || ''
+  if (versionString.match(/\./g) && versionString.match(/\./g).length == 1) {
+    const versionMatch = versionString.match(VERSION_PARSE_REGEX)
+    if (versionMatch) {
+      const [, major = '0', minor = '0', commit = '0'] = versionMatch
 
-    // NB Assumes no major, minor, patch, or commit version exceeds 100
-    return (
-      parseInt(major) * 10000 +
-      parseInt(minor) * 100 +
-      parseInt(patch) +
-      parseInt(commit) / 100
-    )
+      return (
+        parseInt(major) * 10000 + parseInt(minor) * 100 + parseInt(commit) / 100
+      )
+    }
+
+    return 0
+  } else {
+    const versionMatch = versionString.match(OLD_VERSION_PARSE_REGEX)
+    if (versionMatch) {
+      const [
+        ,
+        major = '0',
+        minor = '0',
+        patch = '0',
+        commit = '0'
+      ] = versionMatch
+
+      return (
+        parseInt(major) * 10000 +
+        parseInt(minor) * 100 +
+        parseInt(patch) +
+        parseInt(commit) / 100
+      )
+    }
+
+    return 0
   }
-
-  return 0
 }
 
 /**
@@ -50,6 +67,6 @@ export function workerVersionTestOrInRange(
   min: string,
   max?: string
 ): boolean {
-  const isTestVersion = versionToNumber(version) % 1 !== 0
+  const isTestVersion = (version || '').search(/-/g) !== -1
   return isTestVersion || workerVersionInRange(version, min, max)
 }

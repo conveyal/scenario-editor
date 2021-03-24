d7dbfda1f0319beb2a490f1d4a2a9b9a96b07b3f
Trevor Gerhardt <trevorgerhardt@gmail.com>
2021-03-24 12:47:41 -0600

    Fix slider selector in Cypress

--- cypress/integration/6-analysis.ts
+++ cypress/integration/6-analysis.ts
@@ -42,7 +42,7 @@ describe('Analysis', () => {
   it('has all form elements', function () {
     // note that elements touched in beforeEach are neglected here
     cy.findByLabelText(/Travel time cutoff minutes/)
-    cy.findByRole('slider', {name: /Travel time percentile/i})
+    cy.findByRole('slider', {name: /Percentile/i})
     cy.getPrimaryAnalysisSettings().within(() => {
       cy.findButton(
         'Fetch results with the current settings to enable button'

d5e0f6b21f734259f3b30d5c3a7eb7ae6b8aa313
Trevor Gerhardt <trevorgerhardt@gmail.com>
2021-04-30 08:33:10 +0700

    Decrease the padding for input elements

--- lib/config/chakra.tsx
+++ lib/config/chakra.tsx
@@ -2,7 +2,8 @@ import {ChakraProvider, extendTheme} from '@chakra-ui/react'
 
 const defaultFontFamily = `'Inter', -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`
 
-export const inputPaddingX = 3
+// Horizontal padding for input elements
+export const inputPaddingX = 2
 
 // Lighten font weight
 const Button = {
